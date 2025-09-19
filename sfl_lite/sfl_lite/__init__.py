d517ea508c026822f406947193b0d9cdda2dda31
zoupeicheng.zpc <zoupeicheng.zpc@antgroup.com>
2025-09-19 06:51:49 +0000

    fix license

--- sfl_lite/sfl_lite/__init__.py
+++ sfl_lite/sfl_lite/__init__.py
@@ -9,7 +9,9 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific permissions and limitations under the License.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """SFL Lite: Next-generation federated learning framework."""
 
 __version__ = "0.1.0"
